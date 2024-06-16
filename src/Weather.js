b78e6c1a3b9d6f533dcedcbffc296a075f6d16ce
Andile18 <zama.andile18@gmail.com>
2024-06-16 23:30:44 +0200

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,10 +1,10 @@
 import React from "react";
 import axios from 'axios';
-import Loader from 'react-loader-spinner';
+import { Loader } from 'react-loader-spinner';
+
 
 export default function Weather(props) {
-
-    function handleResponse(response){
+function handleResponse(response){
         alert (`The weather in ${response.data.name} is {response.data.main.temp}`);
     }
     let apiKey = `53f3bc1f5d348c44be3e3754c7185573`;
