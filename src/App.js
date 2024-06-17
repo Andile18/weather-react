13532f33b480962cd74350179e9d2e060d7e55ee
Andile18 <zama.andile18@gmail.com>
2024-06-17 16:56:03 +0200

    edit js, css,index.html

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import Weather from './Weather.js';
+import "./App.css";
+import Search from "../Search";
 
-
-export default function App() {
+function App() {
   return (
-<div className= "App">
-<header className= "App-header"> 
-
-<h1> Hello World!!!! </h1>
-
-
-
-<Weather city="Durban" />
-</header>
-</div>
+    <div className="App">
+      <h1>Weather App</h1>
+      <Search />
+      <footer>
+        This project was coded by Zama Andile Dlamini and is{" "}
+        <a
+          href="https://github.com/Andile18"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          open-source code
+        </a>{" "}
+        on Github and hosted on Netlify
+      </footer>
+    </div>
   );
 }
+
+export default App;
